a746d74dcdafed0e20931c47758933b5c930df8b
Rémy Macherel <remy.macherel@heig-vd.ch>
2021-05-04 16:18:27 +0200

    testExtraction

--- RedressImage.m
+++ RedressImage.m
@@ -4,12 +4,12 @@ function [imRot] = RedressImage(im)
 markers = findMarkers(im);
 upMarker = markers(1:2);
 downMarker = markers(3:end);
-alpha = atan2(downMarker(1)-upMarker(1),downMarker(2)-upMarker(2));
+alpha = atan((downMarker(1)-upMarker(1))/(downMarker(2)-upMarker(2)));
 alphaDeg = -rad2deg(alpha);
 imRot = imrotate(im,alphaDeg);
-back = 255 - imrotate(255*uint8(ones(size(im))),alphaDeg);
-imRot(back == 255)= 255;
-% Mrot = ~imrotate(true(size(im)),alphaDeg);
-% imRot(Mrot&~imclearborder(Mrot)) = 255;
+% replace black background with white
+%Create image with white filling after rotation
+Mask = 255 - imrotate(255*ones(size(im)),alphaDeg);
+imRot(Mask == 255)= 255;
 end
 
--- Remy.m
+++ Remy.m
@@ -2,6 +2,7 @@ clc;clear;close all;
 
 currentFolder = pwd;
 %disp(currentFolder)
+%U-Z_Lavanchy.jpeg   scan3.jpg
 imgPath = strcat(currentFolder,'\Projet_OCR\RawImages\Lavanchy\scan3.jpg');
 im = imread(imgPath);
  %% Get markers and redress image
@@ -17,9 +18,30 @@ upMarker = markers(1:2);
 %reduce image size (makes operations faster) :
 offset = 70;
 imgCrop = imRot(round(upMarker(2)-offset):round(downMarker(2)+offset),:,1);
-figure; imshow(imgCrop)
 
+cropMarkers = findMarkers(imgCrop);
+downMarker = cropMarkers(3:end);
+upMarker = cropMarkers(1:2);
+% figure; imshow(imgCrop)
+% axis on;
+% hold on;
+% plot(round(upMarker(1)),round(upMarker(2)),'r+','MarkerSize',15,'LineWidth',2);
+% hold off
 
+% Extract every case :
+gray = imgCrop;
+Iblob = gray > 100;
+
+nCasesHor = 22;
+nCasesVert = 5;
+
+caseHeigth = 110;
+caseWidth = 80;
+offsetLine = 60;
+a = round(upMarker(1))+offsetLine;
+b = round(upMarker(2))- 55;
+rect1 = gray(b:b+caseHeigth,a: a+caseWidth);
+figure; imshow(rect1)
 
 
 
