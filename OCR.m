93a477e301d28133fb3c0b6518edab66306b4a71
Rémy Macherel <remy.macherel@heig-vd.ch>
2021-05-17 16:28:13 +0200

    Alu

--- OCR.m
+++ OCR.m
@@ -27,7 +27,7 @@ ex = exist(fNameTraining,'file');
 if ex == 2
     load 'trainingDatas.mat';
     Images = trainingDatas{1};
-    labels = char(trainingDatas{2}); % Transform into characters array
+    labels = trainingDatas{2}; 
 else
     [Images,~,labels] = readImages('\Projet_OCR\Training',false);
     saveTrainingDatas(Images,labels);
@@ -41,11 +41,64 @@ end
 %% encoding labels -> Each row of labelsEnc correspond to the code to the label
 %Create column vector 
 %labs = char(labels);
-labs = categorical(labels)
-categories(labs)
-labelsEnc = onehotencode(labs,1)
+% labs = categorical(labels)
+% categories(labs)
+% labelsEnc = onehotencode(labs,1)
+% sample size = 91x69
+% cell2mat(Images{1}) -> Gives 455x1518 (91*5 x 69*22) matrix
+% test = reshape(Images{1}, [1 110]) -> Gives 1x110 cell array containing
+% every sample (character)
+% test1 = reshape(cell2mat(test1), [], numel(test1)); -> Line used to
+% unroll 1x110 cell array into 6279x110 matrix (each column corresponds to
+% a sample
 
-%% Character size = 91x69
+% Example with labels :
+labelsArray = cell2mat(labels);
+% idx = find(labelsArray == 'B');
+% Bimages = Images(idx); % Returns every cell (image) that is labelled with 'B'. Note : every images is a 5x22 cell array containing each character sample
+% Bdatas = [];
+% % Gets a Matrix with every column corresponding to a sample
+% for i= 1: length(Bimages)
+%    temp = reshape(Bimages{i},[110 1]);
+%    for j = 1:length(temp)
+%        Bdatas = [Bdatas, temp{j}(:)];
+%    end
+% end
 
+labelArray = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+datas = [];
+ex = eye(26);
+y = [];
+for k = 1:length(labelArray)
+    idx = find(labelsArray == labelArray(k));
+    if ~isempty(idx)
+        pImages = Images(idx); 
+        for i = 1:length(pImages)
+           temp = reshape(pImages{i}, [110 1]);
+           for j = 1:length(temp)
+              datas = [datas, temp{j}(:)]; 
+              y = [y, ex(:,k)];
+           end
+        end
+    end
+end
 
+%% SHUFFLE
+% r2 = randi(10,1000,1);
+
+%% Creating neural network (cf ex4)
+inputLayerSize = 91*69; % Input layer (size of a letter)
+hiddenLayerSize = 50; % Check how to define size
+numLabels = 26; % From A to Z 
+initial_Theta1 = randInitializeWeights(inputLayerSize, hiddenLayerSize);
+initial_Theta2 = randInitializeWeights(hiddenLayerSize, numLabels);
+EncodedTable = eye(26); % create matrix where every column corresponds to a letter
+% Unroll parameters
+initialParams = [initial_Theta1(:) ; initial_Theta2(:)];
+X = []; % Input
+y = []; % Output vector (or matrix) containing labels
+lambda = 0; % Regularization parameter
+options = optimset('MaxIter',50); 
+costFunction = @(p) nnCostFunction(p,inputLayerSize,hiddenLayerSize,numLabels,X,y,lambda);
+[nnParams,~] = fmincg(costFunction,initialParams,options);
 
